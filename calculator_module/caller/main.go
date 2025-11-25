e61a7109b2cc55e9935c30cb293be6ed4d633cb9
Subham Sahu <about.subhamsahu@gmail.com>
2025-11-26 00:55:46 +0530

    .

---
+++ calculator_module/caller/main.go
@@ -0,0 +1,9 @@
+package caller
+
+import (
+	"github.com/drcount-root/calculator_module/mathx"
+)
+
+func Caller(x, y int) int {
+	return mathx.Add(x, y)
+}
---
+++ calculator_module/go.mod
@@ -0,0 +1,5 @@
+module github.com/drcount-root/calculator_module
+
+go 1.25.4
+
+replace github.com/drcount-root/calculator_module => ../calculator_module
