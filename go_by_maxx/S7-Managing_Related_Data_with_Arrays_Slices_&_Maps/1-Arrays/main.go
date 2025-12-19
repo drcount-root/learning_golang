afb178afe5851a1457e0a032609700bc4f75ab8c
Subham Sahu <about.subhamsahu@gmail.com>
2025-12-19 22:51:35 +0530

    slice

--- go_by_maxx/S7-Managing_Related_Data_with_Arrays_Slices_&_Maps/1-Arrays/main.go
+++ go_by_maxx/S7-Managing_Related_Data_with_Arrays_Slices_&_Maps/1-Arrays/main.go
@@ -17,4 +17,35 @@ func main() {
 	productNames[2] = "Smartwatch"
 	fmt.Printf("3 Product names array after updating value of index 2: %v, which is of length %v\n", productNames, len(productNames))
 
+	/*
+	************* Selecting parts of array using slices *************
+	 */
+
+	names := [7]string{"Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace"}
+
+	namesSlice1 := names[2:5] // returns a slice without modifying the original array
+	fmt.Printf("\nNames slice from index 2 to 4 excluding index 5: %v\n", namesSlice1)
+	fmt.Print("\n", names)
+
+	namesSlice2 := names[:4] // returns a slice from the start to index 3
+	fmt.Printf("\nNames slice from start to index 3 excluding index 4: %v\n", namesSlice2)
+
+	namesSlice3 := names[3:]                                               // returns a slice from index 3 to the end
+	fmt.Printf("\nNames slice from index 3 to the end: %v\n", namesSlice3) // [David Eve Frank Grace]
+
+	namesSlice3[1] = "Evelyn" // modifies the original array as well at the Eve's position
+	fmt.Printf("\nnamesSlice3 after modification: %v\n", namesSlice3)
+	fmt.Printf("\nOriginal names array after modifying namesSlice3: %v\n", names)
+
+	fmt.Printf("Length of names array is %v & capacity of names arrays is %v\n", len(names), cap(names))
+	// Length of names array is 7 & capacity of names arrays is 7
+	// Why is that ? Because arrays have fixed length and capacity equal to their length
+
+	fmt.Printf("\nLength of namesSlice1 is %v & capacity of namesSlice1 is %v\n", len(namesSlice1), cap(namesSlice1))
+	// Length of namesSlice1 is 3 & capacity of namesSlice1 is 5
+	// Why is that ? Because slices have variable length and capacity equal to how many elements can this slice grow to before it runs out of the underlying array.
+
+	// capacity of a slice basically follows this equation:
+	// cap(array[a:b]) = len(array) - a
+	// cap(namesSlice1) = len(names) - 2 = 7 - 2 = 5
 }
