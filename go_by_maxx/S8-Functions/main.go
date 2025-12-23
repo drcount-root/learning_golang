57f46bcb57392ec736e7afa3b62d279a2241816f
Subham Sahu <about.subhamsahu@gmail.com>
2025-12-23 21:59:31 +0530

    functions

--- go_by_maxx/S8-Functions/main.go
+++ go_by_maxx/S8-Functions/main.go
@@ -11,17 +11,23 @@ func main() {
 
 	fmt.Println(arrOfInts) // [0 1 2 3 4 5]
 
-	doubledArr := doubleItFunc(&arrOfInts)
-
+	doubledArr := transformItFunc(&arrOfInts, double)
 	fmt.Println(doubledArr) // [0 2 4 6 8 10]
+
+	tripleArr := transformItFunc(&arrOfInts, triple)
+	fmt.Println(tripleArr) // [0 3 6 9 12 15]
 }
 
-func doubleItFunc(arr *[]int) []int {
+type transformType func(*int) int
+
+// transformType -> this denotes a function that takes a pointer to an int and returns an int
+
+func transformItFunc(arr *[]int, transform transformType) []int {
 	newArr := []int{}
 
 	// here we don't care about the index, thats why we use _
 	for _, value := range *arr {
-		newArr = append(newArr, double(&value))
+		newArr = append(newArr, transform(&value))
 	}
 
 	return newArr
@@ -29,4 +35,8 @@ func doubleItFunc(arr *[]int) []int {
 
 func double(x *int) int {
 	return *x * 2
+}
+
+func triple(x *int) int {
+	return *x * 3
 }
