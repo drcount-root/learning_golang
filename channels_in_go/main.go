ef63e0c746e14f49b4e4c6e61f59e2a999ca74c4
Subham Sahu <about.subhamsahu@gmail.com>
2026-01-12 23:54:54 +0530

    waitgroup - before sync

--- channels_in_go/main.go
+++ channels_in_go/main.go
@@ -7,24 +7,48 @@ import (
 )
 
 func main() {
+	// wg := &sync.WaitGroup{}
+
+	// wg.Add(2)
+
+	// go readerFn(12, wg)
+
+	// go sayHello(wg)
+
+	// wg.Wait()
+
 	wg := &sync.WaitGroup{}
 
-	wg.Add(2)
+	st := time.Now()
 
-	go readerFn(12, wg)
-
-	go sayHello(wg)
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go worker(&i, wg)
+	}
 
 	wg.Wait()
+
+	tt := time.Since(st)
+
+	fmt.Printf("All workers finished their work! Time taken: %v\n", tt)
 }
 
-func readerFn(n int, wg *sync.WaitGroup) {
-	defer wg.Done()
-	fmt.Printf("Reader %v\n", n)
-}
+// func readerFn(n int, wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	fmt.Printf("Reader %v\n", n)
+// }
 
-func sayHello(wg *sync.WaitGroup) {
+// func sayHello(wg *sync.WaitGroup) {
+// 	defer wg.Done()
+// 	time.Sleep(3 * time.Second)
+// 	fmt.Println("sayHello Fn executed after 3 seconds")
+// }
+
+func worker(n *int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	time.Sleep(3 * time.Second)
-	fmt.Println("sayHello Fn executed after 3 seconds")
+	fmt.Printf("\nWorker %v started\n", *n)
+
+	time.Sleep(3 * time.Second) // thing that some work is taking 3 secs to finish
+
+	fmt.Printf("\nWorker %v ended\n", *n)
 }
